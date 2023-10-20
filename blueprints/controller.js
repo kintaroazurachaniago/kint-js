d7c5f93d152a367de405ce03d9f6d61b8deb713e
Kintaro azura chaniago <78691307+kintaroazurachaniago@users.noreply.github.com>
2023-10-20 12:44:35 +0700

    Add files via upload

--- blueprints/controller.js
+++ blueprints/controller.js
@@ -4,5 +4,20 @@ module.exports = (req, res, Model) => {
 	* model used : -={modelName}=-
 	* code...
 	*/
-	res.view('-={name}=-', { title : '-={name}=-' })
+
+	function getMethodHandler () {
+		/* get method handler */
+		res.view('-={name}=-', { title : '-={name}=-', params : req.params })
+	}
+
+	function postMethodHandler () {
+		/* Post method handler */
+		console.log('Form data :', req.body)
+	}
+
+	switch ( req.method ) {
+	case 'GET' : getMethodHandler(); 	break
+	case 'POST': postMethodHandler(); break
+	default    : res.end('Unknown method')
+	}
 }
--- blueprints/main.js
+++ blueprints/main.js
@@ -1,13 +1,40 @@
+const { app, Kint }    = require('kint-js')
 const [ port, status ] = [ 4120, 'App started!' ]
-const { app, Kint } = require('kint-js')
 
 /*
+* #########################
 * use kint object if needed
 * kint class has some method that may usefull for you
 * some of them are .pkg(), .logLeft(), .logRight()
+*/
 
-* Do not remove start and end comment
+/*
+* #############################
+* Using external package method
+* Should be pure function | Cannot run class method properly
+* @params (request, response) - Always bring these 2 important prameter
+* @return <promise> - Executed by async - await function
+* Uncomment some lines of code below to try
+* Don't forget to install it from npmjs.com "npm i node-session"
+*/
 
+// const sess    = require('node-session')
+// const session = new sess({ secret : 'asdf' })
+
+// function useSession (req, res) {
+// 	return new Promise( resolve => {
+// 		session.startSession(req, res, _ => {
+// 			resolve('Session started!')
+// 		})
+// 	})
+// }
+
+// app.use(useSession)
+
+/*
+* #######################################
+* Do not remove "start" and "end" comment
+* #######################################
 */
 
 /* start */
